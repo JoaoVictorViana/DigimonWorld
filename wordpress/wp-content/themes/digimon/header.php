e04629b07b5d46a48c35257d7e11f46b681df3ea
Joao Victor <jv.duarte.viana@gmail.com>
2020-12-30 12:19:45 -0300

    Tema criado

---
+++ wordpress/wp-content/themes/digimon/header.php
@@ -0,0 +1,18 @@
+<?php declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+} 
+
+?>
+
+<!DOCTYPE html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+Teste Header
